76c5f011c878450cc6908afb2b95eb5ad0fe176a
nchaudhary <nchaudhary@homeaway.com>
2017-08-08 13:55:55 -0500

    Initial commit

---
+++ src/main/java/com/example/demoNitesh/DemoNiteshApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoNitesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNiteshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNiteshApplication.class, args);
+	}
+}
